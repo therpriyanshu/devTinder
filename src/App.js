52ac4c8a196a350a5a54b20754d9cabb48db69e8
therpriyanshu <priyanshukr102004@gmail.com>
2025-07-24 12:05:43 +0530

    Get Post

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ const express=require("express")
 const app=express()
 
 //Order matters
-app.use("/hello",(req,res)=>{
-    res.send("Helloo")
-})
-
 app.use("/hello/xyz",(req,res)=>{
     res.send("Hello")
+})
+
+app.use("/hello",(req,res)=>{
+    res.send("Helloo")
 })
 
 
 app.use("/hii",(req,res)=>{
     res.send("Hiiii")
+})
+
+//ab+c , ab*cd , ab?c, a(bc)?d
+app.get("/abc/:userId",(req,res)=>{ // /a/ regex can also work as a path
+    // console.log(req.query)
+    console.log(req.params)
+    res.send({firstName:"Priyanshu" , lastName:"Kumar"})
 })
 
 app.use("/",(req,res)=>{
